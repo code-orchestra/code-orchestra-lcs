8f43d5a581b58ddb1d4b2f3d95d5dbb5fce6eb7e
Alexander Eliseyev <a.a.eliseyev@gmail.com>
2013-04-15 14:54:22 +0700

    [COLT-20]
    Auto-hide fcsh

--- src/codeOrchestra/lcs/ApplicationWorkbenchAdvisor.java
+++ src/codeOrchestra/lcs/ApplicationWorkbenchAdvisor.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchPartReference;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
@@ -15,6 +17,7 @@ import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
 import codeOrchestra.lcs.project.ProjectManager;
 import codeOrchestra.lcs.project.RecentProjects;
+import codeOrchestra.lcs.views.FCSHConsoleView;
 
 /**
  * @author Alexander Eliseyev
@@ -34,23 +37,29 @@ public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
   public String getInitialWindowPerspectiveId() {
     return Perspective.ID;
   }
-  
+
   @Override
   public void initialize(IWorkbenchConfigurer configurer) {
     super.initialize(configurer);
-    
+
     Throwable socketInitException = application.getServerSocketThread().getSocketInitException();
     if (socketInitException != null) {
       MessageDialog.openError(Display.getDefault().getActiveShell(), "Error", "Can't init tracing socket: " + socketInitException);
     }
   }
-  
+
   @Override
   public void postStartup() {
+    // Hide FCSH console
+    IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+    IWorkbenchPartReference myView = page.findViewReference(FCSHConsoleView.ID);
+    page.setPartState(myView, IWorkbenchPage.STATE_MINIMIZED);
+
+    // Open recent project
     List<String> recentProjectsPaths = RecentProjects.getRecentProjectsPaths();
     if (!recentProjectsPaths.isEmpty()) {
       String lastProjectPath = recentProjectsPaths.get(0);
-      
+
       IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
       if (new File(lastProjectPath).exists()) {
         try {
@@ -58,7 +67,7 @@ public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
         } catch (PartInitException e) {
           // TODO: Handle nicely
           e.printStackTrace();
-        }        
+        }
       }
     }
   }
