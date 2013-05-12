07d87953036cc46e1bb817aa326c381036415563
Oleg Chiruhin <hedin.pr@gmail.com>
2013-05-13 05:32:34 +0700

    backport CSS support from e4

--- src/codeOrchestra/lcs/ApplicationWorkbenchAdvisor.java
+++ src/codeOrchestra/lcs/ApplicationWorkbenchAdvisor.java
@@ -3,6 +3,8 @@ package codeOrchestra.lcs;
 import java.io.File;
 import java.util.List;
 
+import org.eclipse.e4.ui.css.swt.theme.IThemeEngine;
+import org.eclipse.e4.ui.css.swt.theme.IThemeManager;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IWorkbenchPage;
@@ -14,6 +16,10 @@ import org.eclipse.ui.application.IWorkbenchConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceReference;
 
 import codeOrchestra.lcs.errorhandling.ErrorHandler;
 import codeOrchestra.lcs.project.ProjectManager;
@@ -49,7 +55,29 @@ public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
     if (socketInitException != null) {
       MessageDialog.openError(Display.getDefault().getActiveShell(), "Error", "Can't init tracing socket: " + socketInitException);
     }
+    
+    setupTheme();
   }
+  
+  	@SuppressWarnings({ "rawtypes", "unchecked", "restriction" })
+	public void setupTheme() {
+		Bundle b = FrameworkUtil.getBundle(getClass());
+		BundleContext context = b.getBundleContext();
+		ServiceReference serviceRef = context
+				.getServiceReference(IThemeManager.class.getName());
+		IThemeManager themeManager = (IThemeManager) context
+				.getService(serviceRef);
+
+		final IThemeEngine engine = themeManager.getEngineForDisplay(Display
+				.getCurrent());
+		engine.setTheme("com.codeorchestra.lcs.themes.dark", true);
+		if (serviceRef != null) {
+			serviceRef = null;
+		}
+		if (themeManager != null) {
+			themeManager = null;
+		}
+	}  
 
   @Override
   public void postStartup() {
