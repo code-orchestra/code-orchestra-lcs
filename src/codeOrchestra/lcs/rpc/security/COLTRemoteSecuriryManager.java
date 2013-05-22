6e3f58725f8a1c6a39b22d57afac1e953a5390cf
Alexander Eliseyev <a.a.eliseyev@gmail.com>
2013-05-22 21:02:45 +0200

    [COLT-184]

--- src/codeOrchestra/lcs/rpc/security/COLTRemoteSecuriryManager.java
+++ src/codeOrchestra/lcs/rpc/security/COLTRemoteSecuriryManager.java
@@ -1,8 +1,14 @@
 package codeOrchestra.lcs.rpc.security;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
+
+import codeOrchestra.utils.StringUtils;
 
 public class COLTRemoteSecuriryManager {
 
@@ -18,20 +24,60 @@ public class COLTRemoteSecuriryManager {
   private static final String TOKENS_KEY = "coltRPCAuthKeys";
   
   private final Preferences preferences = Preferences.userNodeForPackage(COLTRemoteSecuriryManager.class);
-  private Map<String, String> shortCodeToToken = new HashMap<String, String>();
+  
+  private transient Map<String, String> shortCodeToToken = new HashMap<String, String>();
   
   public String createNewTokenAndGetShortCode() {
     // TODO: implement
     return "1234";
   }
   
-  public void addAuthToken() {
-    // TODO: implement
+  public void addAuthToken(String token) {
+    List<String> authTokens = getPersistedAuthTokens();
+    if (!authTokens.contains(authTokens)) {
+      authTokens.add(token);      
+    }
+    
+    storeAuthTokens(authTokens);
   }
   
+  private void storeAuthTokens(List<String> authTokens) {
+    StringBuilder sb = new StringBuilder();    
+    Iterator<String> iterator = authTokens.iterator();
+    while (iterator.hasNext()) {
+      String token = iterator.next();
+      sb.append(token);
+      
+      if (iterator.hasNext()) {
+        sb.append("|");
+      }
+    }
+    preferences.put(TOKENS_KEY, sb.toString());
+    try {
+      preferences.sync();
+    } catch (BackingStoreException e) {
+      throw new RuntimeException("Error while storing auth tokens", e);
+    }
+  }
+
+  private List<String> getPersistedAuthTokens() {
+    List<String> result = new ArrayList<String>();
+    
+    String tokensString = preferences.get(TOKENS_KEY, "");
+    String[] tokensSplit = tokensString.split("\\|");
+    if (tokensSplit != null) {
+      for (String token : tokensSplit) {
+        if (StringUtils.isNotEmpty(token)) {
+          result.add(token);
+        }
+      }
+    }
+    
+    return result;
+  }
+
   public boolean isValidToken(String token) {
-    // TODO: implement
-    return true;
+    return getPersistedAuthTokens().contains(token);
   }
   
 }
