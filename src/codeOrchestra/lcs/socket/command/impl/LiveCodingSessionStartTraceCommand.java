807ad0b582df10c7bf46dd9d30ea1340ca5c93d3
Alexander Eliseyev <a.a.eliseyev@gmail.com>
2013-05-30 18:21:04 +0200

    Clean-up

--- src/codeOrchestra/lcs/socket/command/impl/LiveCodingSessionStartTraceCommand.java
+++ src/codeOrchestra/lcs/socket/command/impl/LiveCodingSessionStartTraceCommand.java
@@ -53,26 +53,5 @@ public class LiveCodingSessionStartTraceCommand implements TraceCommand {
       liveCodingManager.startSession(broadcastId, clientId, clientInfoDictionary, clientSocketHandler);
     }
   }
-  
-  public static void main(String[] args) {
-    String cliendInfo = "A=t&SA=t&SV=t&EV=t&MP3=t&AE=t&VE=t&ACC=f&PR=t&SP=f&SB=f&DEB=t&V=MAC%2011%2C4%2C402%2C287&M=Adobe%20Macintosh&R=1920x1200&COL=color&AR=1.0&OS=Mac%20OS%2010.8.2&ARCH=x86&L=en&PR32=t&PR64=t&PT=StandAlone&AVD=f&LFD=f&WD=t&TLS=t&ML=5.1&DP=72";
-    
-    String decoded = null;
-    try {      
-      decoded = URLDecoder.decode(cliendInfo, "UTF-8");
-    } catch (UnsupportedEncodingException e) {
-      // can't happen
-    }
-    
-    Map<String, String> clientInfoDictionary = new HashMap<String, String>();
-    String[] decodedSplit = decoded.split("&");
-    for (int i = 0; i < decodedSplit.length; i++) {
-      String keyValue = decodedSplit[i];
-      String[] keyValueSplit = keyValue.split("=");      
-      clientInfoDictionary.put(keyValueSplit[0], keyValueSplit[1]);
-    }
-    
-    System.out.println();
-  }
-  
+   
 }
